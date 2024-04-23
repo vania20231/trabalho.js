2e60edb740c93a861806cade9cc9b1c371503b33
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-04-23 08:50:23 -0300

    Create idade.js

---
+++ idade.js
@@ -0,0 +1,12 @@
+let idade = 15;
+if(idade >=18){
+    console.log("sua idade é: " + 
+                 idade +
+    ",  você é maior de idade");
+}else{
+    let idade = 15;
+if(idade >=18)
+    console.log("sua idade é: " + 
+                 idade +
+    ",  você é maenor de idade");
+}
