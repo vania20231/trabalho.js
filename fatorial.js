05952b5e4b1ce3b5f9bd9744a5bc98c2f694d5f9
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-04-23 09:18:34 -0300

    Create fatorial.js

---
+++ fatorial.js
@@ -0,0 +1,14 @@
+const fatorial = 5;
+let resultado = 0;
+resultado = fatorial * (fatorial-1) * (fatorial-2) * (fatorial-3) * (fatorial-4)
+console.log("o resultado é: " + resultado);
+
+const seis  = 6;
+let total = 0;
+total = seis * (seis-1) * (seis-2) * (seis-3) * (seis-4) * (seis-5)
+console.log("o resultado é: " + total);
+
+const dez  = 10;
+let igual = 0;
+igual = dez * (dez-1) * (dez-2) * (dez-3) * (dez-4) * (dez-5) * (dez-6) * (dez-7) * (dez-8) * (dez-9)
+console.log("o resultado é: " + igual);
