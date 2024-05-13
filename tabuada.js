d7c3f354ca5178d76acf2f74326358e56c31fbe6
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-05-13 12:34:21 -0300

    Create tabuada.js

---
+++ tabuada.js
@@ -0,0 +1,21 @@
+console.log("1 x 1 = ", 1 * 1 );
+console.log("1 x 2 = ", 1 * 2 );
+
+console.log(T +"1 x 1 = ", "1" * 1 );
+console.log(T +"1 x 2 = ", "1a" * 2 );
+
+T=1;
+console.log("1 x 1 = ", "1" * 1 );
+console.log("1 x 2 = ", "1a" * 2 );
+
+T = 1;
+M = 1;
+for(T=1; T <= 1; T++) {
+    console.log();
+    console.log("======= T = " + T + "=======")
+    for (M=1; M <= 10;M++) {
+        console.log(T + " x " + M + " = ", T * M );
+
+    }
+
+}
