b206ae1ee27160642458526f7fe3545cd4292f6a
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-05-22 09:01:47 -0300

    Create funçao.js

---
+++ funçao.js
@@ -0,0 +1,13 @@
+console.log("Resultado", soma());
+console.log("Resultado2", soma2(5, 7));
+
+function soma() {
+    x = 5;
+    x = x + 1;
+    return x;
+}
+
+function soma2(a, b){
+    r = a + b;
+    return r;
+}
