95b15e9939e7566ee0c2093ff6575751363a06b5
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-06-05 09:01:16 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,5 @@
+const produtos = ["arroz","feijão","chocolate","bolacha","macarrão","sabonete","miojo","refrigerante","biscoito","pera"]
+console.log(produtos);
+console.log(produtos[3]);
+console.log(produtos.length);
+produtos=[]
