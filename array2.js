d42c1fa12aa8ae31b80524d3f668d731fdfa0082
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-06-11 09:00:23 -0300

    Create array2.js

---
+++ array2.js
@@ -0,0 +1,10 @@
+lista1 = ["Leandro","Maria","Monica"]
+lista2 = ["fassina","silva","fabio"]
+listacompleta = lista1.concat(lista2)
+console.log (listacompleta)
+
+const dado = [["Marcos","Rafaela","Maria"],[8,8.5,9]];
+console.log("nome:"+dado[0][1]+" média: "+dado[1][1]);
+
+const dado2 = [["Marcos","Rafaela","Maria"],[[3,9,7],[5,8,9],[8,9,7]]];
+console.log("nome:"+dado2[0][1]+" média: "+dado2[1][1][0])
