8cc9054cbfd29f9908c14d760fca80eb85806529
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-07-29 11:39:49 -0300

    Create objetopessoa.js

---
+++ objetopessoa.js
@@ -0,0 +1,10 @@
+const objetoPessoaX = {
+    id: 93,
+    nome: "Arisberto",
+    idade: 26,
+    cpf: "999.888.777-66"
+}
+
+console.log(`nome da pessoa: ${objetoPessoaX.nome}`)
+
+console.log(objetoPessoaX)
