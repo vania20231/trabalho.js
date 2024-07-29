171fd141cdd4b685a0264d51d191bab2d57d1b95
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-07-29 11:44:25 -0300

    Create objetocliente.js

---
+++ objetocliente.js
@@ -0,0 +1,10 @@
+const objetoCliente = {
+    id: 5,
+    nome: "Arisberto",
+    idade:26,
+    cpf:"999.888.777-66"
+}
+
+console.log(`Nome: ${objetoCliente['nome']}`)
+console.log(`Idade: ${objetoCliente['idade']}`)
+console.log(`CPF: ${objetoCliente['cpf']}`)
