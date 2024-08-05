bcf8fbebddb10529fb1de52754eefa2770116538
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-05 12:31:30 -0300

    Create objemobj.js

---
+++ objemobj.js
@@ -0,0 +1,17 @@
+const Cliente = {
+    nome: "Arisberto",
+    idade:26,
+    email: "JP@tech.com",
+    telefone:["41 98888-2222", "41 97777-3333"]
+}
+
+Cliente.endereco = {
+    rua: 'Sete de setembro',
+    numero: 954,
+    apartamento: true,
+    complemento: "ap 25"
+}
+
+console.log(Cliente)
+
+console.log(Cliente.endereco)
