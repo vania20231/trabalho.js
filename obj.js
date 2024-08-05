21d8fd288b3cefbd0d130ff6b963c14d78f2ddd1
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-05 12:15:26 -0300

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,13 @@
+const Cliente = {
+    nome: "Arisberto",
+    idade:26,
+    email: "JP@tech.com",
+    telefone:["41 98888-2222", "41 97777-3333"]
+}
+
+
+console.log(Cliente)
+
+console.log(Cliente.telefone)
+
+console.log(Cliente)
