cd289923aead2f89b8fe49910a10224e06b43939
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-06 09:06:38 -0300

    Create listaobj.js

---
+++ listaobj.js
@@ -0,0 +1,27 @@
+const Cliente1 ={
+    nome:"Rafa",
+    idade: 25,
+}
+
+Cliente1.endereco ={
+                    rua:"Brasil",
+                    numero:500,
+}
+
+const Cliente2 = {
+    nome: "Vania",
+    idade: 17,
+    endereco: {
+        rua: "Brasil",
+        numero: 500
+    }
+}
+
+console.log(Cliente1)
+console.log(Cliente2)
+
+
+const lista = ["banana",  "morango",]
+const endereco =[{rua: " "}, {rua: " "}]
+
+endereco[0].rua
