21a6c44c459e7e58ac9d2c4963a206cf06106837
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-19 09:02:33 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,8 @@
+const frutas = ['morango','banana','uva','maça','laranja'];
+
+const frutasMaiusculas = [];
+for (let i =0; i<frutas.length;i++){
+    frutasMaiusculas.push(frutas[i].toUpperCase());
+}
+
+console.log(frutasMaiusculas)
