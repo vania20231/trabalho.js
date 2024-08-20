bbe54e41decc6914486f46b49a33b6f8fbed929a
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-20 10:55:57 -0300

    Create livro.js

---
+++ livro.js
@@ -0,0 +1,10 @@
+const livro = {
+                nome:'1984',
+                autor:'George Orwell',
+                ano:1949,
+                genero:'Distopia',
+                preco:29.90
+                }
+
+
+console.log(livro.nome)
