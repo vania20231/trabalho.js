ef57bc576f402b3ef24cf4fd9fb18de3792772aa
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-21 09:00:11 -0300

    Create turma.js

---
+++ turma.js
@@ -0,0 +1,13 @@
+const turma = {
+                nome:'3ª tds',
+                ano: 2024,
+                professor: {nome:'leandro',
+                            diciplina: 'prog'
+                            },
+                aluno:[{nome:'vania',
+                        idade:17,
+                        },
+                        {nome:'rafa',
+                        idade: 16,
+                }]
+}
